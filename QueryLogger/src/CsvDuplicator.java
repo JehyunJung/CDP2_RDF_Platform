cfa826f15771835341594a1c0f2c69ebf26d1051
jehyunJung <toojey7@gmail.com>
2020-06-20 01:50:07 +0900

    style:change structure of logData Generator

--- QueryLogger/src/CsvDuplicator.java
+++ QueryLogger/src/CsvDuplicator.java
@@ -10,30 +10,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class CsvMerge {
-	public static void mergeCsv(ArrayList<String> sourceFiles,String destFile,int fileNum) {
+public class CsvDuplicator {
+	public static void duplicateCsv(String sourceFile,String destFile) {
 		BufferedReader br = null;
 		BufferedWriter bufWriter = null;
 		String FilePath = "C:\\Users\\jhyun\\Desktop";
-		String queryFileName = "queryset_final";
-		ArrayList<List<List<String>>> allQueries = new ArrayList<>();
-		int sum=0;
-		for(int index=0;index<fileNum;index++) {
-			String sourceFile=sourceFiles.get(index);
-			List<List<String>> allQuery = new ArrayList<List<String>>();
-			int count=0;
-			try {
-				// query 파일에서 query 전체 읽어 allQuery 리스트에 저장
-				br = new BufferedReader(new InputStreamReader(new FileInputStream(FilePath+"\\"+sourceFile+".csv")));
-				String line = "";
-				while((line = br.readLine()) != null){
+		String queryFileName = destFile;
+		List<List<String>> allQuery = new ArrayList<List<String>>();
+		int count=0;
+		try {
+			// query 파일에서 query 전체 읽어 allQuery 리스트에 저장
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(FilePath+"\\"+sourceFile+".csv")));
+			String line = "";
+			while((line = br.readLine()) != null){
 	                //CSV 1행을 저장하는 리스트
 	                List<String> tmpList = new ArrayList<String>();
 	                String temp[] = line.split(",");
 	                String query=temp[1];
 	                //query에 comma가 있는 경우 처리
 	                for(int i=2;i<temp.length;i++) 
-	                	query+=", "+temp[i];
+	                	query+=","+temp[i];
 	                
 	                //double quote 처리
 	                if(query.charAt(0)=='"')
@@ -42,9 +38,8 @@ public class CsvMerge {
 	                //배열에서 리스트 반환
 	                tmpList = Arrays.asList(array);
 	                allQuery.add(tmpList);
-	                sum++;
-	            }
-				allQueries.add(allQuery);
+	                allQuery.add(tmpList);
+				}
 			} catch(FileNotFoundException e) {
 				e.printStackTrace();
 			} catch(IOException e) {
@@ -58,29 +53,12 @@ public class CsvMerge {
 					e.printStackTrace();
 				}
 			}
-		}
 		try {
 			bufWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(FilePath+"\\"+queryFileName+".csv")));
-			int[] start_indexes=new int[fileNum];
-			int[] next_indexes=new int[fileNum];
-			
-			for(int i=0;i<fileNum;i++) {
-				start_indexes[i]=0;
-				next_indexes[i]=(i+1)%fileNum;
-			}
-			
-			int count=0;
-			int index=0;
-			while(count<sum) {
-				List<String> list=allQueries.get(index).get(start_indexes[index]);
-				if(list!=null) {
-					bufWriter.write(list.get(0)+","+list.get(1));
+			for(List<String> query : allQuery) {
+					bufWriter.write(query.get(0)+","+query.get(1));
 					bufWriter.newLine();
-					start_indexes[index]++;
-					count++;
 				}
-				index=next_indexes[index];
-			}
 		} catch (FileNotFoundException e){
             e.printStackTrace();
         } catch (IOException e){
